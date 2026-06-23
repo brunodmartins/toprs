3371f3c319d819a2af73fc684e2e8ebee78e8686
brunodmartins <bdm2943@icloud.com>
2026-06-22 21:33:42 -0300

    Using pretty table

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs;
 use std::path::Path;
+use comfy_table::presets::UTF8_FULL;
+use comfy_table::{Cell, Color, Table};
 
 #[derive(Debug)]
 struct ProcessInfo {
@@ -9,24 +11,37 @@ struct ProcessInfo {
 }
 
 fn main() {
+    let mut processes: Vec<ProcessInfo> = vec![];
     // 1. Listar o diretório /proc
     if let Ok(entries) = fs::read_dir("/proc") {
         for entry in entries.flatten() {
             let path = entry.path();
-            
-            // 2. Filtrar apenas pastas que possuem números como nome (que são os PIDs)
             if path.is_dir() {
                 if let Some(pid_str) = path.file_name().and_then(|s| s.to_str()) {
                     if let Ok(pid) = pid_str.parse::<u32>() {
-                        // 3. Se for um PID válido, extrair os dados
                         if let Some(info) = parse_process_data(pid, &path) {
-                            println!("{:?}", info);
+                            processes.push(info);
                         }
                     }
                 }
             }
         }
     }
+    pretty_print_table(processes);
+}
+
+fn pretty_print_table(processes: Vec<ProcessInfo>) {
+    let mut table = Table::new();
+    table.load_preset(UTF8_FULL);
+    table.set_header(vec!["PID", "NAME", "Memory Usage"]);
+    for process in processes {
+        table.add_row(vec![
+            process.pid.to_string(),
+            process.name,
+            format_mem_display(process.memory_res_kb)
+        ]);
+    }
+    println!("{table}");
 }
 
 fn parse_process_data(pid: u32, proc_path: &Path) -> Option<ProcessInfo> {
@@ -58,4 +73,14 @@ fn get_proc_memory_usage(proc_path: &Path) -> Option<u64> {
     // Convertendo páginas de 4KB para Kilobytes
     let memory_res_kb = res_pages * 4;
     Some(memory_res_kb)
+}
+
+fn format_mem_display(mem_usage_kb: u64) -> String {
+    if mem_usage_kb > 1024 ^ 2 {
+        return format!("{} MB", mem_usage_kb / 1024);
+    }
+    if mem_usage_kb > 1024 ^ 3 {
+        return format!("{} GB", mem_usage_kb / (1024 ^ 2));
+    }
+    return format!("{} KB", mem_usage_kb)
 }
