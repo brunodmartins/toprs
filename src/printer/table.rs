effbeb4d3492a5d6280dc3c969099ed6241f5e87
brunodmartins <bdm2943@icloud.com>
2026-06-27 16:18:41 -0300

    Adding a new summary

--- src/printer/table.rs
+++ src/printer/table.rs
@@ -4,6 +4,11 @@ use comfy_table::Table;
 use comfy_table::presets::UTF8_FULL;
 
 pub fn pretty_print_table(processes: Vec<ProcessInfo>, total_memory: u64) {
+    println!("Summary:");
+    println!("Total Memory available: {}", total_memory);
+    let total_memory_used: u64 = processes.iter().map(|p| p.memory_res_kb).sum();
+    println!("Total Memory used: {} ({}%)", total_memory_used, (total_memory_used * 100) / total_memory);
+    println!("Processes:");
     let mut table = Table::new();
     table.load_preset(UTF8_FULL);
     table.set_header(vec!["PID", "NAME", "Memory Usage","CPU Usage (avg last second)", "CPU Usage (avg since start)"]);
