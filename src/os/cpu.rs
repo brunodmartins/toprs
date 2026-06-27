eacd5ff94161bf9ff82e46b800e040c54c8d8080
brunodmartins <bdm2943@icloud.com>
2026-06-26 23:30:06 -0300

    Remmove core count on CPU usage multiply operation

--- src/os/cpu.rs
+++ src/os/cpu.rs
@@ -35,14 +35,12 @@ pub fn get_cpu_ticks(proc_path: &Path) -> Option<CpuTicks> {
 pub fn get_cpu_usage_last_second(cpu_ticks_init: CpuTicks, cpu_ticks_end: CpuTicks) -> f64 {
     let ticks = cpu_ticks_end.ticks() - cpu_ticks_init.ticks();
     let clk_tck = 100.0;
-    let num_cores = available_parallelism().unwrap().get() as f64;
-    return (ticks / clk_tck) * 100.00 * num_cores
+    return (ticks / clk_tck) * 100.00
 }
 
 pub fn get_cpu_usage_since_start(proc_path: &Path, system_uptime: f64) -> Option<f64> {
     let cpu_ticks = get_cpu_ticks(proc_path).unwrap();
     let clk_tck = 100.0;
-    let num_cores = available_parallelism().unwrap().get() as f64;
 
     let process_start_seconds = cpu_ticks.start_time / clk_tck;
 
@@ -51,7 +49,7 @@ pub fn get_cpu_usage_since_start(proc_path: &Path, system_uptime: f64) -> Option
     // Evita divisão por zero se o processo acabou de nascer
     let cpu_usage_pct = if process_duration_seconds > 0.0 {
         // (Tempo de CPU usado / Tempo total de vida) * 100 * Cores
-        ((cpu_ticks.ticks() / clk_tck) / process_duration_seconds) * 100.0 * num_cores
+        ((cpu_ticks.ticks() / clk_tck) / process_duration_seconds) * 100.0
     } else {
         0.0
     };
